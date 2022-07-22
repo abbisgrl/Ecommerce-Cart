226c7d811f4045ba066698338d43bd1a024da21c
Gyanendra Shah <102413865+abbisgrl@users.noreply.github.com>
2022-07-22 09:07:09 +0530

    CardsDetail Component

--- src/Components/CardsDetail.js
+++ src/Components/CardsDetail.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import Table from 'react-bootstrap/Table';
 
 const CardsDetail = () => {
     return (
         <div className='container mt-2'>
             <h2 className='text-center'>Items Details Page</h2>
-            <section class
+            <section className='container mt-3'>
+                <div className='itemsdetails'>
+                    <div className='items_img'>
+                        <img src='https://b.zmtcdn.com/data/pictures/7/19639627/94c0a4cf15c02d3982d154e2c5dd8cbb_o2_featured_v2.jpg' alt='' />
+                    </div>
+                    <div className='details'>
+                        <Table>
+                            <tr>
+                                <td>
+                                    <p><strong>Restaurant</strong> : Masala Theoryy</p>
+                                    <p><strong>Price</strong> : ₹300</p>
+                                    <p><strong>Dishes</strong> : North Indian,Biryani,Mughlai</p>
+                                    <p><strong>Total</strong> : ₹300</p>
+                                </td>
+                                <td>
+                                    <p><strong>Rating :</strong><span style={{ backgroundColor: 'green', color: '#fff', padding: '2px 5px', borderRadius: '5px' }}> 3.5★</span></p>
+                                    <p><strong>Order Review :</strong><span> 2525 + order placed from here recently</span></p>
+                                    <p><strong>Rating :</strong><span><i className='fas fa-trash' style={{ color: 'red', fontSize: 20, cursor: 'pointer' }}></i></span></p>
+                                </td>
+                            </tr>
+                        </Table>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
